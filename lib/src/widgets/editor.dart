428ef8d9a14377995354990d6930bbcd2fe4b281
hito.cai <hito.cai@mintinglabs.com>
2023-06-01 18:00:58 +0800

    chore: Fully support Dart 3.0

--- lib/src/widgets/editor.dart
+++ lib/src/widgets/editor.dart
@@ -1681,7 +1681,7 @@ class RenderEditor extends RenderEditableContainerBox
   }
 }
 
-class QuillVerticalCaretMovementRun extends Iterator<TextPosition> {
+class QuillVerticalCaretMovementRun implements Iterator<TextPosition> {
   QuillVerticalCaretMovementRun._(
     this._editor,
     this._currentTextPosition,
